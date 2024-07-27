50be01f78f05efccf3e76cb0326de73b54a6872a
Taya Hanchar <tayahanchar@gmail.com>
2024-07-27 21:52:29 +0300

    feat: add icons

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,9 +1,19 @@
 import styles from './Todo.module.css';
+import { TfiClipboard } from "react-icons/tfi";
+import { TfiCheckBox } from "react-icons/tfi";
+import { TfiTrash } from "react-icons/tfi";
 
 function Todo({title, id, deleteTodo}) {
   return (
-    <li className={styles.item}>{title}
-    <button onClick={() => deleteTodo(id)}>del</button>
+    <li className={styles.item}>
+      <span>
+        <TfiClipboard className={styles.icon}/>
+        {title}
+      </span>
+      <span>
+        <TfiCheckBox className={styles.icon} />
+        <TfiTrash className={styles.icon} onClick={() => deleteTodo(id)} />
+      </span>
     </li>
   )
 }
