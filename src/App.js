911edc9920ebb81af722bb9c4817c839758bef59
Taya Hanchar <tayahanchar@gmail.com>
2024-07-27 17:32:36 +0300

    feat: add submit todo and store todos functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 import Form from "./components/Form";
 import List from "./components/List";
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+
+  function addTodo(todo) {
+    setTodoList((prev) => {
+      return [...prev, todo];
+    });
+  }
+
   return (
     <div className="App">
       <h1>TODO app</h1>
-      <Form />
-      <List />
+      <Form addTodo={addTodo} />
+      <List todoList={todoList} />
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import styles from './Form.module.css';
 
-function Form() {
+function Form({addTodo}) {
+
+  const [todo, setTodo] = useState('');
+
+  function changeTodo(event) {
+    setTodo(event.target.value);
+  }
+
+  function submitTodo(event) {
+    event.preventDefault();
+    addTodo({title: todo, isCompleted: false});
+    setTodo('');
+  }
+
   return(
-    <form className={styles.form}>
-      <input className={styles.form__input} type="text" />
+    <form onSubmit={(event) => submitTodo(event)} className={styles.form}>
+      <input onChange={changeTodo} value={todo} className={styles.form__input} type="text" />
       <button className={styles.form__button} type="submit">Submit</button>
     </form>
   )
