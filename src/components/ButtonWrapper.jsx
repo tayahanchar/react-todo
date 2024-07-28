dfbfa9c5f11422b9e418ca5e5279ba7db2b34ac5
Taya Hanchar <tayahanchar@gmail.com>
2024-07-28 19:40:49 +0300

    publick build on gh-pages

--- src/components/ButtonWrapper.jsx
+++
@@ -1,5 +0,0 @@
-function ButtonWrapper(props) {
-  return (<button title={props.title} className={props.className} onClick={props.onClick}>{props.children}</button>)
-}
-
-export default ButtonWrapper;
